869b28b94cb42495b955165f7677e6a35affdbb9
Dorian <dorian.dulac@gmail.com>
2025-08-26 19:23:23 +0200

    Updated Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -71,7 +71,7 @@ const Header = () => {
               Connexion
             </Button>
             <Button 
-              onClick={() => scrollToSection('pricing')}
+              onClick={() => window.location.href = 'https://app.getflaire.fr/login'}
               className="bg-[#FFB23F] hover:bg-[#FF8F00] text-white"
             >
               Essayer gratuitement
