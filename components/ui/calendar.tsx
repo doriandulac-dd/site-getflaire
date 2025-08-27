2442c03b6c35bd8f5c507cc5fd3bcd27d2d6d885
Dorian <dorian.dulac@gmail.com>
2025-08-27 18:30:27 +0200

    Fix Calendar component with correct icon property names

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconArrowLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconArrowRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
