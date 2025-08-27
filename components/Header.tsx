8c2f8e84de4e39b79eff733a85a11e74a7b3c643
Dorian <dorian.dulac@gmail.com>
2025-08-27 17:48:00 +0200

    Supprimer tous les éléments du blog

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,12 +54,6 @@ const Header = () => {
               FAQ
             </button>
             <button 
-              onClick={() => window.location.href = '/blog'}
-              className="text-[#778DA9] hover:text-[#1B263B] font-medium transition-colors"
-            >
-              Blog
-            </button>
-            <button 
               onClick={() => window.location.href = '/contact'}
               className="text-[#778DA9] hover:text-[#1B263B] font-medium transition-colors"
             >
@@ -116,12 +110,6 @@ const Header = () => {
                 className="block w-full text-left px-3 py-2 text-[#778DA9] hover:text-[#1B263B] font-medium"
               >
                 FAQ
-              </button>
-              <button 
-                onClick={() => window.location.href = '/blog'}
-                className="block w-full text-left px-3 py-2 text-[#778DA9] hover:text-[#1B263B] font-medium"
-              >
-                Blog
               </button>
               <button 
                 onClick={() => window.location.href = '/contact'}
