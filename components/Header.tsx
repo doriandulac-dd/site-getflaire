d3fdf4c9b2cf7fac6f01be523304fec127a1478b
Dorian <dorian.dulac@gmail.com>
2025-09-18 23:15:48 +0200

    Updated Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,7 +60,7 @@ const Header = () => {
               Contact
             </button>
             <button 
-              onClick={() => window.location.href = 'https://blog.getflaire.fr'}
+              onClick={() => window.location.href = 'https://leblog.getflaire.fr'}
               className="text-[#778DA9] hover:text-[#1B263B] font-medium transition-colors"
             >
               Blog
@@ -124,7 +124,7 @@ const Header = () => {
                 Contact
               </button>
               <button 
-                onClick={() => window.location.href = 'https://blog.getflaire.fr'}
+                onClick={() => window.location.href = 'https://leblog.getflaire.fr'}
                 className="block w-full text-left px-3 py-2 text-[#778DA9] hover:text-[#1B263B] font-medium"
               >
                 Blog
